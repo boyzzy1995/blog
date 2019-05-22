1ad5f13967d7850fd1869dc7cfe801d6d05324cd
boyzzy1995 <872778957@qq.com>
2019-05-22 13:46:05 +0800

    s

---
+++ hybrid/h5-blog/js/common.js
@@ -0,0 +1,125 @@
+(function() {
+    document.documentElement.style.fontSize = document.documentElement.clientWidth / 640 * 100 + 'px';
+    document.addEventListener('UniAppJSBridgeReady', function() {
+        console.log('UniAppJSBridgeReady', uni);
+    });
+})();
+
+//截取url中需要的参数
+function getUrlParam(name) {
+    let reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)');
+    let r = encodeURI(window.location.search).substr(1).match(reg);
+    if (r != null) return unescape(r[2]);
+    return '';
+}
+
+//加载的弹框
+function loadingTost(type, text) {
+    let $t = $('#loadingToast');
+    if (type == 'show') {
+        if ($t.length > 0) {
+            $('#loadingText').text(text);
+            $t.show();
+        } else {
+            $('body').append(
+                '<div id="loadingToast"  >' +
+                '<div class="weui-mask_transparent"></div>' +
+                '<div class="weui-toast">' +
+                '<i class="weui-loading weui-icon_toast" style="margin: 22px 0 0"></i>' +
+                '<p class="weui-toast__content" id="loadingText">' + text + '</p>' +
+                '</div>' +
+                '</div>');
+            $t.fadeIn(200);
+        }
+    } else {
+        $t.fadeOut(200);
+    }
+}
+
+//完成的提示框
+function Toast(text, time) {
+    let $t = $('#toast');
+    if (!time) {
+        time = 2000;
+    }
+    if ($t.length > 0) {
+        $('#toastText').text(text);
+        $t.show();
+    } else {
+        $('body').append(
+            '<div id="toast" style="opacity: 1; display: none;">' +
+                '<div class="weui-mask_transparent"></div>' +
+                '<div class="weui-toast">' +
+                    '<i class="weui-icon-success-no-circle weui-icon_toast"></i>' +
+                    '<p class="weui-toast__content" id="toastText">' + text + '</p>' +
+                '</div>' +
+            '</div>');
+        $t = $('#toast').fadeIn(200);
+    }
+    setTimeout(() => {
+        $t.fadeOut(200);
+    }, time)
+}
+
+//弹出警告框
+function showWarnToast(text, time) {
+    let $t = $('#warn_toast');
+    if (!time) {
+        time = 2000;
+    }
+    if ($t.length > 0) {
+        $('#warn_toast_Text').text(text);
+        $t.show();
+    } else {
+        $('body').append(
+            '<div id="warn_toast">' +
+            '<div class="weui-mask_transparent"></div>' +
+            '<div class="weui-toast warn-toast">' +
+            '<i class="weui-icon-warn weui-icon_msg-primary my-icon"></i>' +
+            '<p class="weui-toast__content" id="warn_toast_Text">' + text + '</p>' +
+            '</div>' +
+            '</div>');
+        $t = $('#warn_toast').fadeIn(200);
+    }
+    setTimeout(() => {
+        $t.fadeOut(200);
+    }, time)
+}
+//让TextArea自动调节高度
+function makeExpandingArea(el) {
+    let timer = null;
+    //由于ie8有溢出堆栈问题，故调整了这里
+    let setStyle = function(el, auto) {
+        if (auto) el.style.height = '.8rem';
+        el.style.height = el.scrollHeight + 'px';
+    }
+    let delayedResize = function(el) {
+        if (timer) {
+            clearTimeout(timer);
+            timer = null;
+        }
+        timer = setTimeout(function() {
+            setStyle(el)
+        }, 200);
+    }
+    if (el.addEventListener) {
+        el.addEventListener('input', function() {
+            setStyle(el, 1);
+        }, false);
+        setStyle(el)
+    } else if (el.attachEvent) {
+        el.attachEvent('onpropertychange', function() {
+            setStyle(el)
+        })
+        setStyle(el)
+    }
+    if (window.VBArray && window.addEventListener) { //IE9
+        el.attachEvent('onkeydown', function() {
+            let key = window.event.keyCode;
+            if (key == 8 || key == 46) delayedResize(el);
+        });
+        el.attachEvent('oncut', function() {
+            delayedResize(el);
+        }); //处理粘贴
+    }
+}
---
+++ hybrid/h5-blog/pages/blogging/blogging.js
@@ -0,0 +1,80 @@
+let mv = new Vue({
+    el: '#app',
+    data: {
+        formData: {
+            title: '', //标题
+            richText: '', //富文本
+            text: '', //文本
+            pictures: [] //图片数组
+        }
+    },
+    computed: {
+        fontCount() { //计算字的数量
+            let _formData = this.formData;
+            _formData.text = this.pharaSplice(_formData.richText);
+            return _formData.title.length + _formData.text.length;
+        },
+        category() {
+            let category = decodeURI(getUrlParam('category'));
+            return category;
+        }
+    },
+    methods: {
+        pharaSplice(contents) { //只保留文字
+            let str = contents
+                .replace(/\s+/, '') //将多个空字符换成一个空格
+                .replace(/<br\s*?\/?>/, '') //将所有换行符替换成一个字符(不用\n是因为可能会被后面换掉)
+                .replace(/(<\/p>)/, '') //为所有段落添加一个字符(或两个字符，自己定)将点放在前面避免影响后面的替换
+                .replace(/<\/.+?>\s*<[^\/]>/g, '') //去掉所有尾-首相连的HTML标签(包括中间的空字符)
+                .replace(/<.+?>/g, '') //去掉剩下的HTML标签
+                .replace(/&.+?;/g, ''); //转换所有实体为一个字符
+            return str;
+        },
+        judgeForm() { //验证表单
+            if (this.formData.title === '') {
+                showWarnToast('请输入标题');
+                return false;
+            }
+            if (this.formData.text === '' && this.formData.pictures.length === 0) {
+                showWarnToast('请输入内容');
+                return false;
+            }
+            return true;
+        },
+        sumbit() {
+            let array_pic = document.getElementsByTagName('img');
+            for (let i = 0; i < array_pic.length; i++) { //取出图片的base64
+                this.formData.pictures.push(array_pic[i].src);
+            }
+            if(this.judgeForm()) { //判断表单
+                loadingTost('show', '加载中');
+                setTimeout(() => {
+                    loadingTost('hide');
+                    Toast('发布成功');
+                }, 1000)
+            }
+            console.log(this.formData);
+        }
+    },
+    mounted() {
+        
+    }
+})
+$(document).ready(function() {
+    let textarea = document.getElementById('textarea');
+    makeExpandingArea(textarea); //使textArea能够自适应
+    $('#summernote').summernote({ //加载富文本编辑器
+        height: null, // set editor height
+        minHeight: null, // set minimum height of editor
+        maxHeight: null, // set maximum height of editor
+        focus: false, // set focus to editable area after initializin
+        placeholder: '请输入内容',
+        lang: 'zh-CN', // default: 'en-US'
+        dialogsFade: true,
+        callbacks: {
+            onChange: function(contents) { //监听输入
+                mv.formData.richText = contents;
+            }
+        }
+    });
+});
